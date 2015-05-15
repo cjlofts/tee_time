8b8cbd89022cf275290fabe219dd83ac3c6405b4
Ciaran Lofts <cjlofts@sky.com>
2015-05-15 12:37:29 -0700

    adding tooltips to maps

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -7,6 +7,8 @@ module CourseHelper
     Gmaps4rails.build_markers(courses) do |course, marker|
       marker.lat course.latitude
       marker.lng course.longitude
+      link = link_to course.name, golf_course_path(course)
+      marker.infowindow "<h6>#{link}</h6>"
     end
   end
 
